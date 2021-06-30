ea1d4bc66c0a79ee94d2ba0750271f1ceafc0485
Overholtk <overholtkaya@gmail.com>
2021-06-30 09:07:59 -0700

    Add project files.

---
+++ DND_Encounter_WebApp_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DND_Encounter_WebApp_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
